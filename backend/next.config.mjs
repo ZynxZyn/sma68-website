d9b61dc5be5a51b7e7065c25feee22816662c6ae
Nazihan <nazihan@sman68.sch.id>
2026-08-25 12:32:32 +0700

    fix_backend_vercel_build

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ['@prisma/client'],
-  outputFileTracingRoot: new URL('../', import.meta.url).pathname,
 };
 
 export default nextConfig;
+
